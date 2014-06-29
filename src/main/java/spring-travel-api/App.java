dac6be5e3c9b0a1adb1034f98540f303119bdd38
andygodwin <andygodwin@gmail.com>
2014-06-30 00:18:25 +0100

    Initial commit of spring app

---
+++ src/main/java/spring-travel-api/App.java
@@ -0,0 +1,13 @@
+package spring-travel-api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
