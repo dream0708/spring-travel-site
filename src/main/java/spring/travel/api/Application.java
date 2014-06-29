250c7fff5776cb8ce6ff782d92716b4227ff0adf
andygodwin <andygodwin@gmail.com>
2014-06-30 00:31:07 +0100

    * Spring boot dependencies * Main application class

---
+++ src/main/java/spring/travel/api/Application.java
@@ -0,0 +1,23 @@
+package spring.travel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.AsyncRestTemplate;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public AsyncRestTemplate asyncRestTemplate() {
+        return new AsyncRestTemplate();
+    }
+}
