ca56a18ce1608303f63e1179d08a192c83407221
andygodwin <andygodwin@gmail.com>
2014-07-15 08:45:45 +0100

    Offers page

--- src/main/java/spring/travel/site/controllers/OffersController.java
+++ src/main/java/spring/travel/site/controllers/OffersController.java
@@ -20,28 +20,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.ModelAndView;
-import spring.travel.site.compose.AsyncTask;
-import spring.travel.site.compose.ParallelCollector;
-import spring.travel.site.compose.Tuple2;
 import spring.travel.site.model.Advert;
 import spring.travel.site.model.Offer;
 import spring.travel.site.model.user.Loyalty;
 import spring.travel.site.model.user.Profile;
-import spring.travel.site.model.weather.DailyForecast;
-import spring.travel.site.model.weather.Location;
 import spring.travel.site.request.Request;
 import spring.travel.site.request.RequestInfo;
 import spring.travel.site.services.AdvertService;
 import spring.travel.site.services.LoyaltyService;
 import spring.travel.site.services.OffersService;
 import spring.travel.site.services.ProfileService;
-import spring.travel.site.services.WeatherService;
-import spring.travel.site.view.model.HomePage;
+import spring.travel.site.view.model.OffersPage;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -49,8 +41,8 @@ import java.util.Optional;
 import static spring.travel.site.compose.Tasks.parallel;
 
 @Controller
-@RequestMapping("/")
-public class HomeController extends OptionalUserController {
+@RequestMapping("/offers")
+public class OffersController extends OptionalUserController {
 
     @Autowired
     private ProfileService profileService;
@@ -62,39 +54,13 @@ public class HomeController extends OptionalUserController {
     private OffersService offersService;
 
     @Autowired
-    private WeatherService weatherService;
-
-    @Autowired
     private AdvertService advertService;
-
-    @Autowired
-    private GeoLocator geoLocator;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public DeferredResult<ModelAndView> home(@RequestInfo Request requestInfo) {
         return withOptionalUser(requestInfo,
             (request, response) -> {
-
-                ParallelCollector<DailyForecast, Tuple2<Optional<List<Advert>>, Optional<List<Offer>>>> collector =
-                    new ParallelCollector<>(
-                        (result) -> {
-                            Optional<DailyForecast> forecast = result.a();
-
-                            Optional<Tuple2<Optional<List<Advert>>, Optional<List<Offer>>>> adsOffers = result.b();
-                            List<Advert> adverts = adsOffers.flatMap(ao -> ao.a()).orElse(Collections.emptyList());
-                            List<Offer> offers = adsOffers.flatMap(ao -> ao.b()).orElse(Collections.emptyList());
-
-                            Map<String, ?> map = HomePage.from(request.getUser(), offers, forecast, adverts);
-                            response.setResult(new ModelAndView("home", map));
-                        }
-                    );
-
-                Location location = geoLocator.locate(request);
-                AsyncTask<DailyForecast> forecast = weatherService.forecast(location, 5);
-                forecast.onCompletion(
-                    df -> collector.updateA(df)
-                ).execute();
 
                 parallel(
                     profileService.profile(request.getUser()),
@@ -104,10 +70,15 @@ public class HomeController extends OptionalUserController {
                         Optional<Profile> profile = profileLoyalty.flatMap(t -> t.a());
                         Optional<Loyalty> loyalty = profileLoyalty.flatMap(t -> t.b());
                         parallel(
-                            advertService.advert(5, profile),
+                            advertService.advert(4, profile),
                             offersService.offers(profile, loyalty)
                         ).onCompletion(
-                            adsOffers -> collector.updateB(adsOffers)
+                            adsOffers -> {
+                                List<Advert> adverts = adsOffers.get().a().get();
+                                List<Offer> offers = adsOffers.get().b().get();
+                                Map<String, ?> map = OffersPage.from(request.getUser(), offers, adverts);
+                                response.setResult(new ModelAndView("offers", map));
+                            }
                         ).execute();
                     }
                 ).execute();
---
+++ src/main/java/spring/travel/site/view/model/OffersPage.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2014 Andy Godwin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package spring.travel.site.view.model;
+
+import spring.travel.site.model.Advert;
+import spring.travel.site.model.Offer;
+import spring.travel.site.model.user.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class OffersPage {
+
+    public static Map<String, ?> from(Optional<User> user, List<Offer> offers, List<Advert> adverts) {
+        Map<String, Object> map = new HashMap<>();
+        user.ifPresent(u -> map.put("user", u));
+        map.put("offers", OffersView.from(offers, 8));
+        map.put("adverts", AdvertsView.from(adverts, 4));
+        return map;
+    }
+}
