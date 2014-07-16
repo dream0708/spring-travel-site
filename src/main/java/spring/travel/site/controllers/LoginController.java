8e812887945ac4b83400b5d165eb7b52f240a65f
andygodwin <andygodwin@gmail.com>
2014-07-16 15:30:33 +0100

    Changed LoginController from @RestController to plain @Controller

--- src/main/java/spring/travel/site/controllers/LoginController.java
+++ src/main/java/spring/travel/site/controllers/LoginController.java
@@ -19,12 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import spring.travel.site.auth.AuthException;
 import spring.travel.site.auth.PlaySessionCookieBaker;
@@ -37,7 +37,7 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
